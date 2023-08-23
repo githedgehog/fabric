829c7d5b01712e5e3f9021026aaf554a9ac10e1b
Sergei Lukianov <me@slukjanov.name>
2023-08-23 16:10:30 -0700

    Fix API helm chart gen & publish

--- hack/api.mk
+++ hack/api.mk
@@ -1,25 +1,30 @@
+##@ Fabric API (CRDs)
+
 .PHONY: api-lint
 api-lint: kubevious ## Lint all APIs (K8s CRDs) and samples
-	$(KUBEVIOUS) guard config/crd config/samples
+	$(KUBEVIOUS) guard config/crd config/samples/api
+
+.PHONY: api-lint-crds
+api-lint-crds: kubevious ## Lint all APIs (K8s CRDs)
+	$(KUBEVIOUS) guard config/crd
+
+API_HELM ?= config/helm/fabric-api
+API_HELM_PACKAGE ?= $(API_HELM)-$(VERSION).tgz
 
 .PHONY: api-helm-build
-api-helm-build: manifests kustomize helm ## Build Fabric API (CRDs) Helm chart
-	$(KUSTOMIZE) build config/crd > config/helm/fabric-api/templates/crds.yaml
-	$(HELM) package config/helm/fabric-api --destination config/helm --version $(VERSION)
-
-HELM_CRD_PACKAGE ?= config/helm/fabric-api-$(VERSION).tgz
-
-.PHONY: api-helm-lint
-api-helm-lint: helm api-helm-build ## Lint Fabric API (CRDs) Helm chart
-	$(HELM) lint $(HELM_CRD_PACKAGE)
+api-helm-build: manifests kustomize api-lint-crds helm ## Build Fabric API (CRDs) Helm chart
+	rm $(API_HELM)-*.tgz || true
+	$(KUSTOMIZE) build config/crd > $(API_HELM)/templates/crds.yaml
+	$(HELM) package $(API_HELM) --destination config/helm --version $(VERSION)
+	$(HELM) lint $(API_HELM_PACKAGE)
 
 .PHONY: api-helm-push
 api-helm-push: api-helm-build helm ## Push Fabric API (CRDs) Helm chart
-	$(HELM) push $(HELM_CRD_PACKAGE) $(HELM_REPO_URL)
+	$(HELM) push $(API_HELM_PACKAGE) $(HELM_REPO_URL)
 
 .PHONY: api-helm-install
 api-helm-install: api-helm-build helm ## Install Fabric API (CRDs) Helm chart
-	$(HELM) upgrade --install fabric-api $(HELM_CRD_PACKAGE)
+	$(HELM) upgrade --install fabric-api $(API_HELM_PACKAGE)
 
 .PHONY: api-helm-uninstall
 api-helm-uninstall: api-helm-build helm ## Uninstall Fabric API (CRDs) Helm chart
