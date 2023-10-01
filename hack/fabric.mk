c6e440dc1882a163adc06e7ca77943061daa683a
Sergei Lukianov <me@slukjanov.name>
2023-10-01 00:11:21 -0700

    Experiment with parallel build & push in CI

--- hack/fabric.mk
+++ hack/fabric.mk
@@ -83,13 +83,13 @@ fabric-dhcp-server-chart-push-dev: fabric-dhcp-server-chart-build
 	$(HELM) push --insecure-skip-tls-verify config/helm/fabric-dhcp-server-$(VERSION).tgz oci://$(OCI_REPO)/charts
 
 .PHONY: dev-push
-dev-push: api-chart-push-dev fabric-image-push-dev fabric-chart-push-dev agent-push-dev hhfctl-push-dev fabric-dhcp-server-push-dev fabric-dhcp-server-chart-push-dev api-samples
+dev-push: api-chart-push-dev fabric-image-push-dev fabric-chart-push-dev agent-push-dev hhfctl-push-dev fabric-dhcp-server-push-dev fabric-dhcp-server-chart-push-dev
 
 .PHONY: build
-build: api-chart-build fabric-image-build fabric-chart-build agent-build hhfctl-build fabric-dhcp-server-build fabric-dhcp-server-chart-build api-samples
+build: api-chart-build fabric-image-build fabric-chart-build agent-build hhfctl-build fabric-dhcp-server-build fabric-dhcp-server-chart-build
 
 .PHONY: push
-push: api-chart-push fabric-image-push fabric-chart-push agent-push hhfctl-push fabric-dhcp-server-push fabric-dhcp-server-chart-push api-samples
+push: api-chart-push fabric-image-push fabric-chart-push agent-push hhfctl-push fabric-dhcp-server-push fabric-dhcp-server-chart-push
 
 # TODO set dhcp-server image version too (helm set)
 .PHONY: dev-patch
@@ -99,5 +99,5 @@ dev-patch:
 	kubectl patch helmchart fabric-dhcp-server --type=merge -p '{"spec":{"version":"$(VERSION)"}}'
 
 .PHONY: dev
-dev:
+dev: generate manifests api-samples docs
 	VERSION=$(VERSION)-$(shell date +%s) make dev-push dev-patch
