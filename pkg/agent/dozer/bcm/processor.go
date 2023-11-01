11a73738a0b7d3b0141416a3018db830b271203f
Sergei Lukianov <me@slukjanov.name>
2023-11-01 14:25:50 -0700

    Hack to make vxlan work on VS

--- pkg/agent/dozer/bcm/processor.go
+++ pkg/agent/dozer/bcm/processor.go
@@ -55,6 +55,15 @@ func (p *broadcomProcessor) WaitReady(ctx context.Context) error {
 		}
 
 		time.Sleep(3 * time.Second)
+	} 
+
+	// TODO replace with better handling
+	cmd := exec.CommandContext(ctx, "bash", "-c", "(sudo dmidecode -t system | grep 'QEMU') && (sudo iptables -t filter -C INPUT -p udp --dport 4789 -j ACCEPT || sudo iptables -t filter -I INPUT 1 -p udp --dport 4789 -j ACCEPT)")
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stdout
+	err := cmd.Run()
+	if err != nil {
+		return errors.Wrap(err, "failed to fix iptables for vxlan on VS")
 	}
 
 	return nil
