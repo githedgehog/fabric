767890f4c0bab33342bf308effae85c362b56f62
Sergei Lukianov <me@slukjanov.name>
2024-03-07 19:21:37 -0800

    Add missed virtual edge file

---
+++ api/agent/v1alpha2/virtualedge.go
@@ -0,0 +1,16 @@
+package v1alpha2
+
+const (
+	VIRTUAL_EDGE_ANNOTATION = "virtual-edge.hhfab.fabric.githedgehog.com/external-cfg"
+)
+
+type VirtualEdgeConfig struct {
+	ASN          string `json:"ASN"`
+	VRF          string `json:"VRF"`
+	CommunityIn  string `json:"CommunityIn"`
+	CommunityOut string `json:"CommunityOut"`
+	NeighborIP   string `json:"NeighborIP"`
+	IfName       string `json:"ifName"`
+	IfVlan       string `json:"ifVlan"`
+	IfIP         string `json:"ifIP"`
+}
