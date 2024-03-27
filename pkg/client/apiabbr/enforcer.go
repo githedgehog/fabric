fb6d9c82bfb2b57247acc9c678cbaf33165443e6
Sergei Lukianov <me@slukjanov.name>
2024-03-27 13:01:27 -0700

    [abbr] add simple api state enforcer

---
+++ pkg/client/apiabbr/enforcer.go
@@ -0,0 +1,351 @@
+// Copyright 2023 Hedgehog
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package apiabbr
+
+import (
+	"context"
+	"log/slog"
+	"slices"
+	"strings"
+
+	"github.com/pkg/errors"
+	"go.githedgehog.com/fabric/api/meta"
+	vpcapi "go.githedgehog.com/fabric/api/vpc/v1alpha2"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	ctrlutil "sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+)
+
+type Enforcer struct {
+	handlers []AbbrHandler
+}
+
+func NewEnforcer(cleanupNotDefined bool) (*Enforcer, error) {
+	e := &Enforcer{}
+
+	vpc, err := newVPCHandler(cleanupNotDefined)
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to create VPC abbr handler")
+	}
+
+	vpcAttachment, err := newVPCAttachmentHandler(cleanupNotDefined)
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to create VPCAttachment abbr handler")
+	}
+
+	vpcPeering, err := newVPCPeeringHandler(cleanupNotDefined)
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to create VPCPeering abbr handler")
+	}
+
+	fallback, err := newConnectionFallbackHandler()
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to create Connection fallback abbr handler")
+	}
+
+	e.handlers = []AbbrHandler{
+		vpc,
+		vpcAttachment,
+		vpcPeering,
+		fallback,
+	}
+
+	return e, nil
+}
+
+func (e *Enforcer) Load(lines ...string) error {
+	for _, line := range lines {
+		abbrs := strings.Fields(line)
+		for _, abbr := range abbrs {
+			parts := strings.Split(abbr, ":")
+			if len(parts) < 1 {
+				return errors.Errorf("invalid abbr: %s", abbr)
+			}
+
+			abbrType := AbbrType(parts[0])
+			if !slices.Contains(AbbrTypes, abbrType) {
+				abbrType = AbbrTypeUnknown
+			} else {
+				parts = parts[1:]
+			}
+
+			abbrPart := parts[0]
+			parts = parts[1:]
+
+			params := AbbrParams{}
+			for _, part := range parts {
+				kv := strings.SplitN(part, "=", 2)
+
+				key := kv[0]
+				val := TrueVals[0]
+				if len(kv) == 2 {
+					val = kv[1]
+				}
+
+				if _, ok := params[key]; !ok {
+					params[key] = []string{}
+				}
+				params[key] = append(params[key], val)
+			}
+
+			handles := 0
+			for _, handler := range e.handlers {
+				if handled, err := handler.Load(abbrType, abbrPart, params); err != nil {
+					return errors.Wrapf(err, "failed to load abbr: %s", abbr)
+				} else if handled {
+					handles++
+				}
+			}
+
+			if handles == 0 {
+				return errors.Errorf("no handler found for abbr: %s", abbr)
+			} else if handles > 1 {
+				return errors.Errorf("multiple handlers found for abbr: %s", abbr)
+			}
+		}
+	}
+
+	return nil
+}
+
+func (e *Enforcer) Enforce(ctx context.Context, kube client.Client) error {
+	for i := len(e.handlers) - 1; i >= 0; i-- {
+		handler := e.handlers[i]
+		if err := handler.PreProcess(ctx, kube); err != nil {
+			return errors.Wrapf(err, "failed to process deletes")
+		}
+	}
+
+	for _, handler := range e.handlers {
+		if err := handler.PostProcess(ctx, kube); err != nil {
+			return errors.Wrapf(err, "failed to process updates")
+		}
+	}
+
+	return nil
+}
+
+type AbbrType string
+
+const (
+	AbbrTypeUnknown            AbbrType = ""
+	AbbrTypeVPC                AbbrType = "vpc"
+	AbbrTypeVPCAttachment      AbbrType = "vpcAttach"
+	AbbrTypeVPCPeering         AbbrType = "vpcPeering"
+	AbbrTypeExternalPeering    AbbrType = "extPeering"
+	AbbrTypeConnectionFallback AbbrType = "fallback"
+)
+
+var AbbrTypes = []AbbrType{
+	AbbrTypeVPC,
+	AbbrTypeConnectionFallback,
+}
+
+var (
+	TrueVals        = []string{"true", "t", "yes", "y", "1"}
+	TrueValsDefault = append(TrueVals, "")
+)
+
+type AbbrParams map[string][]string
+
+func (p AbbrParams) GetBool(keys []string) (bool, error) {
+	ret := false
+	found := false
+
+	for _, key := range keys {
+		if vals, ok := p[key]; ok {
+			if found {
+				return false, errors.Errorf("multiple key aliases for key %s", key)
+			}
+
+			if len(vals) > 1 {
+				return false, errors.Errorf("multiple values for key %s", key)
+			}
+
+			ret = slices.Contains(TrueVals, vals[0])
+			found = true
+		}
+	}
+
+	return ret, nil
+}
+
+func (p AbbrParams) GetString(keys []string) (string, error) {
+	var ret string
+	found := false
+
+	for _, key := range keys {
+		if vals, ok := p[key]; ok {
+			if found {
+				return "", errors.Errorf("multiple key aliases for key %s", key)
+			}
+
+			if len(vals) > 1 {
+				return "", errors.Errorf("multiple values for key %s", key)
+			}
+
+			ret = vals[0]
+			found = true
+		}
+	}
+
+	return ret, nil
+}
+
+func (p AbbrParams) GetStringSlice(keys []string) []string {
+	var ret []string
+
+	for _, key := range keys {
+		if vals, ok := p[key]; ok {
+			ret = append(ret, vals...)
+		}
+	}
+
+	return ret
+}
+
+type AbbrHandler interface {
+	Load(ct AbbrType, abbr string, params AbbrParams) (handled bool, err error)
+	PreProcess(ctx context.Context, kube client.Client) error
+	PostProcess(ctx context.Context, kube client.Client) error
+}
+
+type ObjectAbbrHandler[T meta.Object, TList meta.ObjectList] struct {
+	AbbrType         AbbrType
+	AcceptedParams   [][]string
+	AcceptNoTypeFn   func(abbr string) bool
+	NameFn           func(abbr string) (name string)
+	ParseObjectFn    func(name, abbr string, params AbbrParams) (T, error)
+	ObjectListFn     func(ctx context.Context, kube client.Client) (TList, error)
+	CreateOrUpdateFn func(ctx context.Context, kube client.Client, obj T) (ctrlutil.OperationResult, error)
+	PatchExistingFn  func(obj T) bool
+
+	DisallowOverride  bool
+	CleanupNotDefined bool
+
+	objs map[string]T
+}
+
+var _ AbbrHandler = (*ObjectAbbrHandler[*vpcapi.VPC, *vpcapi.VPCList])(nil)
+
+func (h *ObjectAbbrHandler[T, TList]) Init() (*ObjectAbbrHandler[T, TList], error) {
+	if h.AbbrType == AbbrTypeUnknown {
+		return nil, errors.New("AbbrType is required")
+	}
+	if h.ParseObjectFn == nil {
+		return nil, errors.New("ParseObjectFn is required")
+	}
+	if h.ObjectListFn == nil {
+		return nil, errors.New("ObjectListFn is required")
+	}
+	if h.CreateOrUpdateFn == nil {
+		return nil, errors.New("CreateOrUpdateFn is required")
+	}
+
+	h.objs = map[string]T{}
+
+	return h, nil
+}
+
+func (h *ObjectAbbrHandler[T, TList]) Load(ct AbbrType, abbr string, params AbbrParams) (bool, error) {
+	if ct != AbbrTypeUnknown && ct != h.AbbrType {
+		return false, nil
+	}
+
+	if ct == AbbrTypeUnknown && (h.AcceptNoTypeFn == nil || !h.AcceptNoTypeFn(abbr)) {
+		return false, nil
+	}
+
+	name := abbr
+	if h.NameFn != nil {
+		name = h.NameFn(abbr)
+	}
+
+	obj, err := h.ParseObjectFn(name, abbr, params)
+	if err != nil {
+		return false, errors.Wrapf(err, "failed to parse object for command: %s", abbr)
+	}
+
+	_, exists := h.objs[obj.GetName()]
+	if h.DisallowOverride && exists {
+		return false, errors.Errorf("object already defined: %s", name)
+	}
+
+	h.objs[obj.GetName()] = obj
+
+	return true, nil
+}
+
+func (h *ObjectAbbrHandler[T, TList]) PreProcess(ctx context.Context, kube client.Client) error {
+	if !h.CleanupNotDefined {
+		return nil
+	}
+
+	list, err := h.ObjectListFn(ctx, kube)
+	if err != nil {
+		return errors.Wrapf(err, "failed to list objects: %s", list.GetObjectKind().GroupVersionKind().Kind)
+	}
+
+	for _, obj := range list.GetItems() {
+		name := obj.GetName()
+		if _, ok := h.objs[name]; !ok {
+			kind := obj.GetObjectKind().GroupVersionKind().Kind
+			slog.Debug("deleting not defined object", "kind", kind, "name", name)
+			if err := kube.Delete(ctx, obj); err != nil {
+				return errors.Wrapf(err, "failed to delete object: %s/%s", kind, name)
+			}
+
+			slog.Info("object deleted", "kind", kind, "name", name)
+		}
+	}
+
+	return nil
+}
+
+func (h *ObjectAbbrHandler[T, TList]) PostProcess(ctx context.Context, kube client.Client) error {
+	if h.PatchExistingFn != nil {
+		list, err := h.ObjectListFn(ctx, kube)
+		if err != nil {
+			return errors.Wrapf(err, "failed to list objects: %s", list.GetObjectKind().GroupVersionKind().Kind)
+		}
+
+		for _, obj := range list.GetItems() {
+			if typed, ok := obj.(T); ok {
+				if h.PatchExistingFn(typed) {
+					name, kind := obj.GetName(), obj.GetObjectKind().GroupVersionKind().Kind
+					slog.Debug("patching existing object", "kind", kind, "name", name)
+					if err := kube.Update(ctx, typed); err != nil {
+						return errors.Wrapf(err, "failed to patch object: %s/%s", kind, name)
+					}
+
+					slog.Info("patched existing object", "kind", kind, "name", name)
+				}
+			} else {
+				return errors.Errorf("object is not of expected type")
+			}
+		}
+	}
+
+	for _, obj := range h.objs {
+		name, kind := obj.GetName(), obj.GetObjectKind().GroupVersionKind().Kind
+		slog.Debug("enforcing object", "kind", kind, "name", name)
+		if res, err := h.CreateOrUpdateFn(ctx, kube, obj); err != nil {
+			return errors.Wrapf(err, "failed to enforce object: %s/%s", kind, name)
+		} else if res != ctrlutil.OperationResultNone {
+			slog.Info("object "+string(res), "kind", kind, "name", name)
+		}
+	}
+
+	return nil
+}
---
+++ pkg/client/apiabbr/vpc.go
@@ -0,0 +1,381 @@
+// Copyright 2023 Hedgehog
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package apiabbr
+
+import (
+	"context"
+	"slices"
+	"strings"
+
+	"github.com/pkg/errors"
+	vpcapi "go.githedgehog.com/fabric/api/vpc/v1alpha2"
+	wiringapi "go.githedgehog.com/fabric/api/wiring/v1alpha2"
+	"go.githedgehog.com/fabric/pkg/util/pointer"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	ctrlutil "sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+)
+
+var (
+	VPCParamDefaultIsolated   = []string{"defaultIsolated", "defI", "i"}
+	VPCParamDefaultRestricted = []string{"defaultRestricted", "defR", "r"}
+	VPCParamIPNS              = []string{"ipns"}
+	VPCParamVLANNS            = []string{"vlanns"}
+	VPCParamSubnets           = []string{"subnet", "s"}
+	VPCParamPermits           = []string{"permit", "p"}
+
+	VPCParams = [][]string{
+		VPCParamDefaultIsolated,
+		VPCParamDefaultRestricted,
+		VPCParamIPNS,
+		VPCParamVLANNS,
+		VPCParamSubnets,
+		VPCParamPermits,
+	}
+)
+
+func newVPCHandler(cleanupNotDefined bool) (*ObjectAbbrHandler[*vpcapi.VPC, *vpcapi.VPCList], error) {
+	return (&ObjectAbbrHandler[*vpcapi.VPC, *vpcapi.VPCList]{
+		AbbrType:          AbbrTypeVPC,
+		CleanupNotDefined: cleanupNotDefined,
+		AcceptedParams:    VPCParams,
+		AcceptNoTypeFn: func(abbr string) bool {
+			return strings.HasPrefix(abbr, "vpc-") && !strings.Contains(abbr, VPCAttachmentAbbrSeparator) && !strings.Contains(abbr, VPCPeeringAbbrSeparator)
+		},
+		ParseObjectFn: func(name, _ string, params AbbrParams) (*vpcapi.VPC, error) {
+			spec := vpcapi.VPCSpec{
+				Subnets: map[string]*vpcapi.VPCSubnet{},
+			}
+			var err error
+
+			if spec.DefaultIsolated, err = params.GetBool(VPCParamDefaultIsolated); err != nil {
+				return nil, err
+			}
+			if spec.DefaultRestricted, err = params.GetBool(VPCParamDefaultRestricted); err != nil {
+				return nil, err
+			}
+			if spec.IPv4Namespace, err = params.GetString(VPCParamIPNS); err != nil {
+				return nil, err
+			}
+			if spec.VLANNamespace, err = params.GetString(VPCParamVLANNS); err != nil {
+				return nil, err
+			}
+
+			for _, subnetRaw := range params.GetStringSlice(VPCParamSubnets) {
+				name, subnet, err := ParseVPCSubnet(subnetRaw)
+				if err != nil {
+					return nil, errors.Wrapf(err, "failed to parse subnet: %s", subnetRaw)
+				}
+
+				spec.Subnets[name] = subnet
+			}
+
+			for _, permitRaw := range params.GetStringSlice(VPCParamPermits) {
+				permit, err := ParseVPCPermits(permitRaw)
+				if err != nil {
+					return nil, errors.Wrapf(err, "failed to parse permit entry: %s", permitRaw)
+				}
+
+				spec.Permit = append(spec.Permit, permit)
+			}
+
+			return &vpcapi.VPC{
+				TypeMeta:   metav1.TypeMeta{APIVersion: vpcapi.GroupVersion.String(), Kind: vpcapi.KindVPC},
+				ObjectMeta: metav1.ObjectMeta{Name: name, Namespace: metav1.NamespaceDefault},
+				Spec:       spec,
+			}, nil
+		},
+		ObjectListFn: func(ctx context.Context, kube client.Client) (*vpcapi.VPCList, error) {
+			list := &vpcapi.VPCList{}
+
+			return list, kube.List(ctx, list)
+		},
+		CreateOrUpdateFn: func(ctx context.Context, kube client.Client, newObj *vpcapi.VPC) (ctrlutil.OperationResult, error) {
+			// TODO if no subnets assigned to the VPC, consider auto allocate some from the used IP namespace
+
+			vpc := &vpcapi.VPC{ObjectMeta: newObj.ObjectMeta}
+
+			return ctrlutil.CreateOrUpdate(ctx, kube, vpc, func() error {
+				vpc.Spec = newObj.Spec
+
+				return nil
+			})
+		},
+	}).Init()
+}
+
+const (
+	VPCAttachmentAbbrSeparator = "@"
+)
+
+var (
+	VPCAttachmentParamNativeVLAN = []string{"native-vlan", "native", "nv"}
+
+	VPCAttachmentParams = [][]string{
+		VPCAttachmentParamNativeVLAN,
+	}
+)
+
+func newVPCAttachmentHandler(cleanupNotDefined bool) (*ObjectAbbrHandler[*vpcapi.VPCAttachment, *vpcapi.VPCAttachmentList], error) {
+	return (&ObjectAbbrHandler[*vpcapi.VPCAttachment, *vpcapi.VPCAttachmentList]{
+		AbbrType:          AbbrTypeVPCAttachment,
+		CleanupNotDefined: cleanupNotDefined,
+		AcceptedParams:    VPCAttachmentParams,
+		AcceptNoTypeFn:    func(abbr string) bool { return strings.Contains(abbr, "@") },
+		NameFn: func(abbr string) string {
+			return strings.ReplaceAll(abbr, VPCAttachmentAbbrSeparator, "--")
+		},
+		ParseObjectFn: func(name, abbr string, params AbbrParams) (*vpcapi.VPCAttachment, error) {
+			spec := vpcapi.VPCAttachmentSpec{}
+
+			parts := strings.Split(abbr, VPCAttachmentAbbrSeparator)
+			if len(parts) != 2 {
+				return nil, errors.New("VPCAttachment abbr should contain exactly vpc/subnet and connection (or server) name separated by " + VPCAttachmentAbbrSeparator)
+			}
+
+			spec.Subnet = parts[0]
+			spec.Connection = parts[1]
+
+			var err error
+			if spec.NativeVLAN, err = params.GetBool(VPCAttachmentParamNativeVLAN); err != nil {
+				return nil, err
+			}
+
+			return &vpcapi.VPCAttachment{
+				TypeMeta:   metav1.TypeMeta{APIVersion: vpcapi.GroupVersion.String(), Kind: vpcapi.KindVPCAttachment},
+				ObjectMeta: metav1.ObjectMeta{Name: name, Namespace: metav1.NamespaceDefault},
+				Spec:       spec,
+			}, nil
+		},
+		ObjectListFn: func(ctx context.Context, kube client.Client) (*vpcapi.VPCAttachmentList, error) {
+			list := &vpcapi.VPCAttachmentList{}
+
+			return list, kube.List(ctx, list)
+		},
+		CreateOrUpdateFn: func(ctx context.Context, kube client.Client, newObj *vpcapi.VPCAttachment) (ctrlutil.OperationResult, error) {
+			conn := &wiringapi.Connection{}
+			if err := kube.Get(ctx, client.ObjectKey{Name: newObj.Spec.Connection, Namespace: metav1.NamespaceDefault}, conn); err != nil {
+				if client.IgnoreNotFound(err) != nil {
+					return ctrlutil.OperationResultNone, errors.Wrapf(err, "cannot get connection")
+				}
+
+				serverName := newObj.Spec.Connection
+				srv := &wiringapi.Server{}
+				if err := kube.Get(ctx, client.ObjectKey{Name: serverName, Namespace: metav1.NamespaceDefault}, srv); err != nil {
+					return ctrlutil.OperationResultNone, errors.Wrapf(err, "cannot get server %s", serverName)
+				}
+
+				connList := &wiringapi.ConnectionList{}
+				if err := kube.List(ctx, connList, wiringapi.MatchingLabelsForListLabelServer(serverName)); err != nil {
+					return ctrlutil.OperationResultNone, errors.Wrapf(err, "cannot list connections for server %s", serverName)
+				}
+
+				if len(connList.Items) == 0 {
+					return ctrlutil.OperationResultNone, errors.Errorf("no connections found for the server %s", serverName)
+				} else if len(connList.Items) > 1 {
+					return ctrlutil.OperationResultNone, errors.Errorf("multiple connections found for the server %s", serverName)
+				}
+
+				newObj.Spec.Connection = connList.Items[0].Name
+			}
+
+			attachment := &vpcapi.VPCAttachment{ObjectMeta: newObj.ObjectMeta}
+
+			return ctrlutil.CreateOrUpdate(ctx, kube, attachment, func() error {
+				attachment.Spec = newObj.Spec
+
+				return nil
+			})
+		},
+	}).Init()
+}
+
+const (
+	VPCPeeringAbbrSeparator = "+"
+)
+
+var (
+	VPCPeeringParamRemote = []string{"remote", "r"}
+	VPCPeeringParamPermit = []string{"permit", "p"}
+
+	VPCPeeringParams = [][]string{
+		VPCPeeringParamRemote,
+		VPCPeeringParamPermit,
+	}
+)
+
+func newVPCPeeringHandler(cleanupNotDefined bool) (*ObjectAbbrHandler[*vpcapi.VPCPeering, *vpcapi.VPCPeeringList], error) {
+	return (&ObjectAbbrHandler[*vpcapi.VPCPeering, *vpcapi.VPCPeeringList]{
+		AbbrType:          AbbrTypeVPCPeering,
+		CleanupNotDefined: cleanupNotDefined,
+		AcceptedParams:    VPCPeeringParams,
+		AcceptNoTypeFn:    func(abbr string) bool { return strings.Contains(abbr, "+") },
+		NameFn: func(abbr string) string {
+			return strings.ReplaceAll(abbr, VPCPeeringAbbrSeparator, "--")
+		},
+		ParseObjectFn: func(name, abbr string, params AbbrParams) (*vpcapi.VPCPeering, error) {
+			spec := vpcapi.VPCPeeringSpec{
+				Permit: []map[string]vpcapi.VPCPeer{},
+			}
+			var err error
+
+			vpcNames := strings.Split(abbr, VPCPeeringAbbrSeparator)
+			if len(vpcNames) != 2 {
+				return nil, errors.New("VPCPeering abbr should contain exactly two VPC names separated by " + VPCPeeringAbbrSeparator)
+			}
+
+			if spec.Remote, err = params.GetString(VPCPeeringParamRemote); err != nil {
+				return nil, err
+			}
+
+			for _, permitRaw := range params.GetStringSlice(VPCPeeringParamPermit) {
+				permit, err := ParseVPCPeeringPermits(vpcNames[0], vpcNames[1], name)
+				if err != nil {
+					return nil, errors.Wrapf(err, "failed to parse permit entry: %s", permitRaw)
+				}
+
+				spec.Permit = append(spec.Permit, permit)
+			}
+
+			return &vpcapi.VPCPeering{
+				TypeMeta:   metav1.TypeMeta{APIVersion: vpcapi.GroupVersion.String(), Kind: vpcapi.KindVPCPeering},
+				ObjectMeta: metav1.ObjectMeta{Name: name, Namespace: metav1.NamespaceDefault},
+				Spec:       spec,
+			}, nil
+		},
+		ObjectListFn: func(ctx context.Context, kube client.Client) (*vpcapi.VPCPeeringList, error) {
+			list := &vpcapi.VPCPeeringList{}
+
+			return list, kube.List(ctx, list)
+		},
+		CreateOrUpdateFn: func(ctx context.Context, kube client.Client, newObj *vpcapi.VPCPeering) (ctrlutil.OperationResult, error) {
+			peering := &vpcapi.VPCPeering{ObjectMeta: newObj.ObjectMeta}
+
+			return ctrlutil.CreateOrUpdate(ctx, kube, peering, func() error {
+				peering.Spec = newObj.Spec
+
+				return nil
+			})
+		},
+	}).Init()
+}
+
+func ParseVPCSubnet(in string) (string, *vpcapi.VPCSubnet, error) {
+	parts := strings.Split(in, ",")
+
+	name := ""
+	subnet := &vpcapi.VPCSubnet{}
+
+	for idx, part := range parts {
+		part := strings.TrimSpace(part)
+
+		kv := strings.Split(part, "=")
+		if len(kv) == 0 || len(kv) > 2 {
+			return "", nil, errors.Errorf("invalid key-value pair: %s (expected k=v or k)", part)
+		}
+
+		key := strings.TrimSpace(kv[0])
+		value := ""
+		if len(kv) == 2 {
+			value = strings.TrimSpace(kv[1])
+		}
+
+		trueVal := slices.Contains(TrueValsDefault, value)
+
+		if idx == 0 {
+			name = key
+			subnet.Subnet = value
+		} else if key == "vlan" {
+			subnet.VLAN = value
+		} else if key == "isolated" || key == "i" {
+			if trueVal {
+				subnet.Isolated = pointer.To(true)
+			}
+		} else if key == "restricted" || key == "r" {
+			if trueVal {
+				subnet.Restricted = pointer.To(true)
+			}
+		} else if key == "dhcp" {
+			if trueVal {
+				subnet.DHCP.Enable = true
+			}
+		} else if key == "dhcp-start" {
+			if subnet.DHCP.Range == nil {
+				subnet.DHCP.Range = &vpcapi.VPCDHCPRange{}
+			}
+			subnet.DHCP.Range.Start = value
+		} else if key == "dhcp-end" {
+			if subnet.DHCP.Range == nil {
+				subnet.DHCP.Range = &vpcapi.VPCDHCPRange{}
+			}
+			subnet.DHCP.Range.End = value
+		} else if key == "dhcp-relay" {
+			subnet.DHCP.Relay = value
+		} else if key == "dhcp-pxe-url" {
+			subnet.DHCP.PXEURL = value
+		} else {
+			return "", nil, errors.Errorf("unknown key: %s", key)
+		}
+	}
+
+	if name == "" {
+		return "", nil, errors.New("subnet name is required")
+	}
+
+	return name, subnet, nil
+}
+
+func ParseVPCPermits(in string) ([]string, error) {
+	parts := strings.Split(in, ",")
+
+	if len(parts) < 2 {
+		return nil, errors.New("permit entry should contain at least two subnets")
+	}
+
+	permits := []string{}
+
+	for _, part := range parts {
+		permits = append(permits, strings.TrimSpace(part))
+	}
+
+	return permits, nil
+}
+
+func ParseVPCPeeringPermits(vpc1, vpc2, in string) (map[string]vpcapi.VPCPeer, error) {
+	vpcParts := strings.Split(in, "~")
+
+	if len(vpcParts) != 2 {
+		return nil, errors.New("permit entry should contain exactly two VPCs")
+	}
+
+	vpc1Subnets := strings.Split(vpcParts[0], ",")
+	if len(vpc1Subnets) == 1 && vpc1Subnets[0] == "" {
+		vpc1Subnets = nil
+	}
+
+	vpc2Subnets := strings.Split(vpcParts[1], ",")
+	if len(vpc2Subnets) == 1 && vpc2Subnets[0] == "" {
+		vpc2Subnets = nil
+	}
+
+	return map[string]vpcapi.VPCPeer{
+		vpc1: {
+			Subnets: vpc1Subnets,
+		},
+		vpc2: {
+			Subnets: vpc2Subnets,
+		},
+	}, nil
+}
