67145c51d7afc4e07e3b168b05d35d474ce88771
Frostman <134872+Frostman@users.noreply.github.com>
2025-05-13 01:51:56 +0000

    bump: gateway to v0.6.1
    This is an automated commit created by GitHub Actions workflow,
    in the gateway repository.
    
    Signed-off-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- vendor/go.githedgehog.com/gateway/api/gateway/v1alpha1/gateway_types.go
+++ vendor/go.githedgehog.com/gateway/api/gateway/v1alpha1/gateway_types.go
@@ -5,6 +5,8 @@ package v1alpha1
 
 import (
 	"context"
+	"fmt"
+	"net/netip"
 
 	kmetav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	kclient "sigs.k8s.io/controller-runtime/pkg/client"
@@ -13,7 +15,30 @@ import (
 // NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
 
 // GatewaySpec defines the desired state of Gateway.
-type GatewaySpec struct{}
+type GatewaySpec struct {
+	// ProtocolIP is used as a loopback IP and BGP Router ID
+	ProtocolIP string `json:"protocolIP,omitempty"`
+	// ASN is the ASN of the gateway
+	ASN uint32 `json:"asn,omitempty"`
+	// Interfaces is a map of interface names to their configurations
+	Interfaces map[string]GatewayInterface `json:"interfaces,omitempty"`
+	// Neighbors is a list of BGP neighbors
+	Neighbors []GatewayBGPNeighbor `json:"neighbors,omitempty"`
+}
+
+// GatewayInterface defines the configuration for a gateway interface
+type GatewayInterface struct {
+	// IP is the IP address to assign to the interface
+	IP string `json:"ip,omitempty"`
+}
+
+// GatewayBGPNeighbor defines the configuration for a BGP neighbor
+type GatewayBGPNeighbor struct {
+	// IP is the IP address of the BGP neighbor
+	IP string `json:"ip,omitempty"`
+	// ASN is the remote ASN of the BGP neighbor
+	ASN uint32 `json:"asn,omitempty"`
+}
 
 // GatewayStatus defines the observed state of Gateway.
 type GatewayStatus struct{}
@@ -44,10 +69,59 @@ func init() {
 }
 
 func (gw *Gateway) Default() {
-	// TODO add defaulting logic
 }
 
 func (gw *Gateway) Validate(_ context.Context, _ kclient.Reader) error {
-	// TODO add validation logic
+	protoIP, err := netip.ParsePrefix(gw.Spec.ProtocolIP)
+	if err != nil {
+		return fmt.Errorf("invalid ProtocolIP %s: %w", gw.Spec.ProtocolIP, err)
+	}
+	if protoIP.Bits() != 32 {
+		return fmt.Errorf("ProtocolIP %s must be a /32 prefix", gw.Spec.ProtocolIP) //nolint:goerr113
+	}
+	if !protoIP.Addr().Is4() {
+		return fmt.Errorf("ProtocolIP %s must be an IPv4 address", gw.Spec.ProtocolIP) //nolint:goerr113
+	}
+
+	if gw.Spec.ASN == 0 {
+		return fmt.Errorf("ASN must be set") //nolint:goerr113
+	}
+
+	if len(gw.Spec.Interfaces) == 0 {
+		return fmt.Errorf("at least one interface must be defined") //nolint:goerr113
+	}
+	for name, iface := range gw.Spec.Interfaces {
+		if iface.IP == "" {
+			return fmt.Errorf("interface %s must have an IP address", name) //nolint:goerr113
+		}
+		ifaceIP, err := netip.ParsePrefix(iface.IP)
+		if err != nil {
+			return fmt.Errorf("invalid interface IP %s: %w", iface.IP, err)
+		}
+		if !ifaceIP.Addr().Is4() {
+			return fmt.Errorf("interface %s IP %s must be an IPv4 address", name, iface.IP) //nolint:goerr113
+		}
+	}
+
+	if len(gw.Spec.Neighbors) == 0 {
+		return fmt.Errorf("at least one BGP neighbor must be defined") //nolint:goerr113
+	}
+	for _, neigh := range gw.Spec.Neighbors {
+		if neigh.IP == "" {
+			return fmt.Errorf("BGP neighbor must have an IP address") //nolint:goerr113
+		}
+		neighIP, err := netip.ParseAddr(neigh.IP)
+		if err != nil {
+			return fmt.Errorf("invalid neighbor IP %s: %w", neigh.IP, err)
+		}
+		if !neighIP.Is4() {
+			return fmt.Errorf("BGP neighbor %s IP %s must be an IPv4 address", neigh.IP, neigh.IP) //nolint:goerr113
+		}
+
+		if neigh.ASN == 0 {
+			return fmt.Errorf("BGP neighbor %s must have an ASN", neigh.IP) //nolint:goerr113
+		}
+	}
+
 	return nil
 }
