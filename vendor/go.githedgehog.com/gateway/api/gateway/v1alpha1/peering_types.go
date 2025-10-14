ade20da9ff2c0066c442669601b3952a4e4c8da3
Frostman <134872+Frostman@users.noreply.github.com>
2025-10-14 04:06:38 +0000

    bump: gateway to v0.20.0
    This is an automated commit created by GitHub Actions workflow,
    in the gateway repository.
    
    Signed-off-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- vendor/go.githedgehog.com/gateway/api/gateway/v1alpha1/peering_types.go
+++ vendor/go.githedgehog.com/gateway/api/gateway/v1alpha1/peering_types.go
@@ -7,6 +7,7 @@ import (
 	"context"
 	"fmt"
 	"maps"
+	"net/netip"
 	"slices"
 
 	kmetav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -102,6 +103,33 @@ func (p *Peering) Validate(_ context.Context, _ kclient.Reader) error {
 	vpcs := slices.Collect(maps.Keys(p.Spec.Peering))
 	if len(vpcs) != 2 {
 		return fmt.Errorf("peering must have exactly 2 VPCs, got %d", len(vpcs)) //nolint:goerr113
+	}
+	for name, vpc := range p.Spec.Peering {
+		if vpc == nil {
+			continue
+		}
+		for _, expose := range vpc.Expose {
+			for _, ip := range expose.IPs {
+				if _, err := netip.ParsePrefix(ip.CIDR); err != nil {
+					return fmt.Errorf("invalid CIDR %s in peering expose IPs of VPC %s: %w", ip.CIDR, name, err)
+				}
+				if ip.Not != "" {
+					if _, err := netip.ParsePrefix(ip.Not); err != nil {
+						return fmt.Errorf("invalid Not CIDR %s in peering expose IPs of VPC %s: %w", ip.Not, name, err)
+					}
+				}
+			}
+			for _, as := range expose.As {
+				if _, err := netip.ParsePrefix(as.CIDR); err != nil {
+					return fmt.Errorf("invalid CIDR %s in peering expose AS of VPC %s: %w", as.CIDR, name, err)
+				}
+				if as.Not != "" {
+					if _, err := netip.ParsePrefix(as.Not); err != nil {
+						return fmt.Errorf("invalid Not CIDR %s in peering expose AS of VPC %s: %w", as.Not, name, err)
+					}
+				}
+			}
+		}
 	}
 
 	return nil
