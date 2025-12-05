5b9e22286c8863638659b4f365b27f08f25b8c75
Emanuele Di Pascale <emanuele@githedgehog.com>
2025-12-05 15:00:14 +0100

    feat(agent): support IPv6 enable
    Signed-off-by: Emanuele Di Pascale <emanuele@githedgehog.com>

--- pkg/agent/dozer/dozer.go
+++ pkg/agent/dozer/dozer.go
@@ -135,6 +135,7 @@ type SpecSubinterface struct {
 	VLAN            *uint16                     `json:"vlan,omitempty"`
 	IPs             map[string]*SpecInterfaceIP `json:"ips,omitempty"`
 	AnycastGateways []string                    `json:"anycastGateways,omitempty"`
+	Ipv6Enabled     *bool                       `json:"ipv6Enabled,omitempty"`
 }
 
 type SpecMCLAGDomain struct {
