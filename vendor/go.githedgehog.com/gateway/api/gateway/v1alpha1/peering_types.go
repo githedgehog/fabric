9206abf97769fa03c00a2820fb227340502dc2a9
Frostman <134872+Frostman@users.noreply.github.com>
2026-02-11 22:45:49 +0000

    bump: gateway to v0.41.0
    This is an automated commit created by GitHub Actions workflow,
    in the gateway repository.
    
    Signed-off-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- vendor/go.githedgehog.com/gateway/api/gateway/v1alpha1/peering_types.go
+++ vendor/go.githedgehog.com/gateway/api/gateway/v1alpha1/peering_types.go
@@ -19,7 +19,8 @@ import (
 )
 
 const (
-	DefaultStatefulIdleTimeout = 2 * time.Minute
+	DefaultMasqueradeIdleTimeout  = 2 * time.Minute
+	DefaultPortForwardIdleTimeout = 2 * time.Minute
 )
 
 // NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
@@ -32,18 +33,47 @@ type PeeringSpec struct {
 	Peering map[string]*PeeringEntry `json:"peering,omitempty"`
 }
 
-type PeeringStatefulNAT struct {
+type PeeringNATMasquerade struct {
 	// Time since the last packet after which flows are removed from the connection state table
 	IdleTimeout kmetav1.Duration `json:"idleTimeout,omitempty"`
 }
 
-type PeeringStatelessNAT struct{}
+// +kubebuilder:validation:Enum=tcp;udp;""
+type PeeringNATProtocol string
+
+const (
+	// Any protocol by default
+	PeeringNATProtocolAny PeeringNATProtocol = ""
+	// TCP only
+	PeeringNATProtocolTCP PeeringNATProtocol = "tcp"
+	// UDP only
+	PeeringNATProtocolUDP PeeringNATProtocol = "udp"
+)
+
+var PeeringNATProtocols = []PeeringNATProtocol{
+	PeeringNATProtocolAny,
+	PeeringNATProtocolTCP,
+	PeeringNATProtocolUDP,
+}
+
+type PeeringNATPortForwardEntry struct {
+	Protocol PeeringNATProtocol `json:"proto,omitempty"`
+	Port     string             `json:"port,omitempty"`
+	As       string             `json:"as,omitempty"`
+}
+
+type PeeringNATPortForward struct {
+	// Time since the last packet after which flows are removed from the connection state table
+	IdleTimeout kmetav1.Duration             `json:"idleTimeout,omitempty"`
+	Ports       []PeeringNATPortForwardEntry `json:"ports,omitempty"`
+}
+
+type PeeringNATStatic struct{}
 
 type PeeringNAT struct {
-	// Use connection state tracking when performing NAT
-	Stateful *PeeringStatefulNAT `json:"stateful,omitempty"`
-	// Use connection state tracking when performing NAT, use stateful NAT if omitted
-	Stateless *PeeringStatelessNAT `json:"stateless,omitempty"`
+	Masquerade  *PeeringNATMasquerade  `json:"masquerade,omitempty"`
+	PortForward *PeeringNATPortForward `json:"portForward,omitempty"`
+	Static      *PeeringNATStatic      `json:"static,omitempty"`
 }
 
 type PeeringEntryExpose struct {
@@ -55,9 +85,6 @@ type PeeringEntryExpose struct {
 
 type PeeringEntry struct {
 	Expose []PeeringEntryExpose `json:"expose,omitempty"`
-	// Ingress []PeeringEntryIngress `json:"ingress,omitempty"`
-	// TODO add natType: stateful # as there are not enough IPs in the "as" pool
-	// TODO add metric: 0 # add 0 to the advertised route metrics
 }
 
 type PeeringEntryIP struct {
@@ -67,8 +94,6 @@ type PeeringEntryIP struct {
 	Not string `json:"not,omitempty"`
 	// CIDR by VPC subnet name to include, only one of cidr, not, vpcSubnet can be set
 	VPCSubnet string `json:"vpcSubnet,omitempty"`
-	// Port ranges (e.g. "80, 443, 3000-3100"), used together with exactly one of cidr, not, vpcSubnet
-	Ports string `json:"ports,omitempty"`
 }
 
 type PeeringEntryAs struct {
@@ -76,21 +101,7 @@ type PeeringEntryAs struct {
 	CIDR string `json:"cidr,omitempty"`
 	// CIDR to exclude, only one of cidr, not can be set
 	Not string `json:"not,omitempty"`
-	// Port ranges (e.g. "80, 443, 3000-3100"), used together with exactly one of cidr, not
-	Ports string `json:"ports,omitempty"`
 }
-
-// type PeeringEntryIngress struct {
-// Allow *PeeringEntryIngressAllow `json:"allow,omitempty"`
-// TODO add deny?
-// }
-
-// type PeeringEntryIngressAllow struct {
-// TODO add actual fields
-// stateless: true
-// 	 tcp:
-//     srcPort: 443
-// }
 
 // PeeringStatus defines the observed state of Peering.
 type PeeringStatus struct{}
@@ -136,9 +147,33 @@ func (p *Peering) Default() {
 	p.Labels[ListLabelVPC(vpcs[1])] = ListLabelValue
 
 	for _, peering := range p.Spec.Peering {
-		// This will modify p.Spec.Peering because peering is *Peering
-		for i := range peering.Expose {
-			peering.Expose[i].Default()
+		for idx := range peering.Expose {
+			expose := &peering.Expose[idx]
+
+			if len(expose.As) != 0 {
+				if expose.NAT == nil {
+					expose.NAT = &PeeringNAT{}
+				}
+			}
+
+			nat := expose.NAT
+			if nat != nil {
+				if nat.Static == nil && nat.Masquerade == nil && nat.PortForward == nil {
+					nat.Static = &PeeringNATStatic{}
+				}
+
+				if nat.Masquerade != nil {
+					if nat.Masquerade.IdleTimeout.Duration == 0 {
+						nat.Masquerade.IdleTimeout.Duration = DefaultMasqueradeIdleTimeout
+					}
+				}
+
+				if nat.PortForward != nil {
+					if nat.PortForward.IdleTimeout.Duration == 0 {
+						nat.PortForward.IdleTimeout.Duration = DefaultPortForwardIdleTimeout
+					}
+				}
+			}
 		}
 	}
 
@@ -147,39 +182,14 @@ func (p *Peering) Default() {
 	}
 }
 
-func (e *PeeringEntryExpose) Default() {
-	if len(e.As) != 0 {
-		if e.NAT == nil {
-			e.NAT = &PeeringNAT{}
-		}
-		e.NAT.Default()
-	}
-}
-
-func (n *PeeringNAT) Default() {
-	if n.Stateful == nil && n.Stateless == nil {
-		n.Stateless = &PeeringStatelessNAT{}
-	}
-
-	if n.Stateful != nil {
-		n.Stateful.Default()
-	}
-}
-
-func (s *PeeringStatefulNAT) Default() {
-	if s.IdleTimeout.Duration == 0 {
-		s.IdleTimeout.Duration = DefaultStatefulIdleTimeout
-	}
-}
-
 func (p *Peering) Validate(ctx context.Context, kube kclient.Reader) error {
 	if p.Spec.GatewayGroup == "" {
-		return fmt.Errorf("gateway group must be specified %s", p.Name) //nolint:goerr113
+		return fmt.Errorf("gateway group must be specified %s", p.Name) //nolint:err113
 	}
 
 	vpcs := slices.Collect(maps.Keys(p.Spec.Peering))
 	if len(vpcs) != 2 {
-		return fmt.Errorf("peering must have exactly 2 VPCs, got %d", len(vpcs)) //nolint:goerr113
+		return fmt.Errorf("peering must have exactly 2 VPCs, got %d", len(vpcs)) //nolint:err113
 	}
 	for name, vpc := range p.Spec.Peering {
 		if vpc == nil {
@@ -187,10 +197,10 @@ func (p *Peering) Validate(ctx context.Context, kube kclient.Reader) error {
 		}
 		for _, expose := range vpc.Expose {
 			if expose.DefaultDestination && (len(expose.IPs) > 0 || len(expose.As) > 0 || expose.NAT != nil) {
-				return fmt.Errorf("default flag should be the only thing set in expose of VPC %s", name) //nolint:goerr113
+				return fmt.Errorf("default flag should be the only thing set in expose of VPC %s", name) //nolint:err113
 			}
 			if len(expose.IPs) == 0 && !expose.DefaultDestination {
-				return fmt.Errorf("at least one IP block must be specified in peering expose of VPC %s", name) //nolint:goerr113
+				return fmt.Errorf("at least one IP block must be specified in peering expose of VPC %s", name) //nolint:err113
 			}
 			for _, ip := range expose.IPs {
 				nonnil := 0
@@ -210,11 +220,7 @@ func (p *Peering) Validate(ctx context.Context, kube kclient.Reader) error {
 					nonnil++
 				}
 				if nonnil != 1 {
-					return fmt.Errorf("exactly one of cidr, not or vpcSubnet must be set in peering expose IPs of VPC %s", name) //nolint:goerr113
-				}
-
-				if err := validatePorts(ip.Ports); err != nil {
-					return fmt.Errorf("invalid ports %s in peering expose IPs of VPC %s: %w", ip.Ports, name, err)
+					return fmt.Errorf("exactly one of cidr, not or vpcSubnet must be set in peering expose IPs of VPC %s", name) //nolint:err113
 				}
 			}
 			for _, as := range expose.As {
@@ -232,35 +238,48 @@ func (p *Peering) Validate(ctx context.Context, kube kclient.Reader) error {
 					nonnil++
 				}
 				if nonnil != 1 {
-					return fmt.Errorf("exactly one of cidr or not must be set in peering expose AS of VPC %s", name) //nolint:goerr113
-				}
-
-				if err := validatePorts(as.Ports); err != nil {
-					return fmt.Errorf("invalid ports %s in peering expose AS of VPC %s: %w", as.Ports, name, err)
+					return fmt.Errorf("exactly one of cidr or not must be set in peering expose AS of VPC %s", name) //nolint:err113
 				}
 			}
 
 			if (len(expose.As) == 0) != (expose.NAT == nil) {
-				return fmt.Errorf("expose.As and expose.NAT must both be set or both be empty in peering expose of VPC %s", name) //nolint:goerr113
+				return fmt.Errorf("expose.As and expose.NAT must both be set or both be empty in peering expose of VPC %s", name) //nolint:err113
 			}
 
 			if expose.NAT != nil {
-				nonnil := 0
-				if expose.NAT.Stateless != nil {
-					// TODO(mvachhar) validate that stateless NAT has the same number of IPs in the ips and as blocks
-					nonnil++
+				nonNils := 0
+				if expose.NAT.Static != nil {
+					nonNils++
+				}
+				if expose.NAT.Masquerade != nil {
+					nonNils++
+				}
+				if expose.NAT.PortForward != nil {
+					nonNils++
 				}
 
-				if expose.NAT.Stateful != nil {
-					nonnil++
+				if nonNils != 1 {
+					return fmt.Errorf("exactly one of masquerade, static, or portForward must be set in NAT section for peering expose of VPC %s", name) //nolint:err113
 				}
 
-				if nonnil == 0 {
-					return fmt.Errorf("expose.NAT must have at least one of stateful or stateless set in peering expose of VPC %s", name) //nolint:goerr113
-				}
+				if expose.NAT.PortForward != nil {
+					if len(expose.NAT.PortForward.Ports) == 0 {
+						return fmt.Errorf("at least one port forwarding rule must be set in NAT section for peering expose of VPC %s", name) //nolint:err113
+					}
 
-				if nonnil > 1 {
-					return fmt.Errorf("only one of statefulNat or statelessNat can be set in peering expose of VPC %s", name) //nolint:goerr113
+					for idx, entry := range expose.NAT.PortForward.Ports {
+						if err := validatePort(entry.Port); err != nil {
+							return fmt.Errorf("invalid port %q in port forwarding rule %d in NAT section for peering expose of VPC %s: %w", entry.Port, idx, name, err)
+						}
+
+						if err := validatePort(entry.As); err != nil {
+							return fmt.Errorf("invalid as %q in port forwarding rule %d in NAT section for peering expose of VPC %s: %w", entry.As, idx, name, err)
+						}
+
+						if !slices.Contains(PeeringNATProtocols, entry.Protocol) {
+							return fmt.Errorf("invalid protocol %q in port forwarding rule %d in NAT section for peering expose of VPC %s", entry.Protocol, idx, name) //nolint:err113
+						}
+					}
 				}
 			}
 		}
@@ -354,54 +373,52 @@ func collectExposedCIDRs(entry *PeeringEntry, cidrs []string) []string {
 	return cidrs
 }
 
-func validatePorts(in string) error {
-	if in == "" {
-		return nil
+func validatePort(in string) error {
+	if strings.TrimSpace(in) != in {
+		return fmt.Errorf("invalid port %q: should not contain leading or trailing whitespace", in) //nolint:err113
 	}
 
-	// TODO probably normalize in Default() and check for overlapping ranges/duplicates
+	if strings.Contains(in, ",") {
+		return fmt.Errorf("invalid port %q: should be a single port or range", in) //nolint:err113
+	}
 
-	for ports := range strings.SplitSeq(in, ",") {
-		ports = strings.TrimSpace(ports)
+	switch {
+	case in == "":
+		return fmt.Errorf("port entry should not be empty") //nolint:err113
+	case !strings.Contains(in, "-"):
+		if port, err := strconv.Atoi(in); err != nil {
+			return fmt.Errorf("invalid port %q: %w", in, err)
+		} else if port < 1 || port > 65535 {
+			return fmt.Errorf("invalid port %d: port should be between 1 and 65535", port) //nolint:err113
+		}
+	default:
+		parts := strings.Split(in, "-")
+		if len(parts) != 2 {
+			return fmt.Errorf("invalid port range %s: should be in format start-end", in) //nolint:err113
+		}
 
-		switch {
-		case ports == "":
-			return fmt.Errorf("port entry should not be empty") //nolint:err113
-		case !strings.Contains(ports, "-"):
-			if port, err := strconv.Atoi(ports); err != nil {
-				return fmt.Errorf("invalid port %s: %w", ports, err)
-			} else if port < 1 || port > 65535 {
-				return fmt.Errorf("invalid port %d: port should be between 1 and 65535", port) //nolint:err113
-			}
-		default:
-			parts := strings.Split(ports, "-")
-			if len(parts) != 2 {
-				return fmt.Errorf("invalid port range %s: should be in format start-end", ports) //nolint:err113
-			}
+		parts[0] = strings.TrimSpace(parts[0])
+		parts[1] = strings.TrimSpace(parts[1])
+		if parts[0] == "" || parts[1] == "" {
+			return fmt.Errorf("invalid port range %s: both start and end should not be empty", in) //nolint:err113
+		}
 
-			parts[0] = strings.TrimSpace(parts[0])
-			parts[1] = strings.TrimSpace(parts[1])
-			if parts[0] == "" || parts[1] == "" {
-				return fmt.Errorf("invalid port range %s: both start and end should not be empty", ports) //nolint:err113
-			}
+		start, err := strconv.Atoi(parts[0])
+		if err != nil {
+			return fmt.Errorf("invalid start port %s: %w", parts[0], err)
+		} else if start < 1 || start > 65535 {
+			return fmt.Errorf("invalid start port %d: port should be between 1 and 65535", start) //nolint:err113
+		}
 
-			start, err := strconv.Atoi(parts[0])
-			if err != nil {
-				return fmt.Errorf("invalid start port %s: %w", parts[0], err)
-			} else if start < 1 || start > 65535 {
-				return fmt.Errorf("invalid start port %d: port should be between 1 and 65535", start) //nolint:err113
-			}
+		end, err := strconv.Atoi(parts[1])
+		if err != nil {
+			return fmt.Errorf("invalid end port %s: %w", parts[1], err)
+		} else if end < 1 || end > 65535 {
+			return fmt.Errorf("invalid end port %d: port should be between 1 and 65535", end) //nolint:err113
+		}
 
-			end, err := strconv.Atoi(parts[1])
-			if err != nil {
-				return fmt.Errorf("invalid end port %s: %w", parts[1], err)
-			} else if end < 1 || end > 65535 {
-				return fmt.Errorf("invalid end port %d: port should be between 1 and 65535", end) //nolint:err113
-			}
-
-			if start > end {
-				return fmt.Errorf("invalid port range %s: start port %d is greater than end port %d", ports, start, end) //nolint:err113
-			}
+		if start > end {
+			return fmt.Errorf("invalid port range %s: start port %d is greater than end port %d", in, start, end) //nolint:err113
 		}
 	}
 
