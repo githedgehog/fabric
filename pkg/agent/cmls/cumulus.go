9cdd41e1c9a43f08c223c163cf29466bcc687135
Sergei Lukianov <me@slukjanov.name>
2026-03-06 11:34:09 -0800

    feat(cmls): full support for ref arch (except p2p links)
    Signed-off-by: Sergei Lukianov <me@slukjanov.name>

--- pkg/agent/cmls/cumulus.go
+++ pkg/agent/cmls/cumulus.go
@@ -12,6 +12,8 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
+	"slices"
+	"strconv"
 	"strings"
 	"text/template"
 	"time"
@@ -37,9 +39,20 @@ var fullCfgTmpl string
 
 type ConfigIn struct {
 	Hostname     string
-	ManagementIP string
 	Users        []User
 	NTPServer    string
+	ManagementIP string
+	ProtocolIP   string
+	VTEPIP       string
+	ASN          uint32
+	HostSubnet   string
+	RouterID     string
+	VXLANSource  string
+	VPCs         []VPC
+	BGPNeighbors []BGPNeighbor
+	PortConfigs  []PortConfig
+	IsSpine      bool
+	IsLeaf       bool
 }
 
 type User struct {
@@ -54,10 +67,42 @@ type SSHKey struct {
 	Type string
 }
 
+type BGPNeighbor struct {
+	IP          string
+	PeerGroup   string
+	Description string
+}
+
+type VPC struct {
+	Name   string
+	VNI    uint32
+	Subnet string
+}
+
+type PortConfig struct {
+	Name            string
+	VRF             string
+	IP              string
+	AdaptiveRouting bool
+}
+
 func buildConfigFor(tmpl string, agent *agentapi.Agent) (*bytes.Buffer, error) {
+	isSpine := agent.IsSpineLeaf() && agent.Spec.Switch.Role.IsSpine()
+	isLeaf := agent.IsSpineLeaf() && agent.Spec.Switch.Role.IsLeaf()
+
 	controlVIP, err := netip.ParsePrefix(agent.Spec.Config.ControlVIP)
 	if err != nil {
 		return nil, fmt.Errorf("parsing control VIP: %w", err)
+	}
+
+	protocolIP, err := netip.ParsePrefix(agent.Spec.Switch.ProtocolIP)
+	if err != nil {
+		return nil, fmt.Errorf("parsing protocol IP: %w", err)
+	}
+
+	vtepIP, err := netip.ParsePrefix(agent.Spec.Switch.VTEPIP)
+	if agent.IsSpineLeaf() && agent.Spec.Switch.Role.IsLeaf() && err != nil {
+		return nil, fmt.Errorf("parsing VTEP IP: %w", err)
 	}
 
 	users := []User{}
@@ -95,11 +140,159 @@ func buildConfigFor(tmpl string, agent *agentapi.Agent) (*bytes.Buffer, error) {
 		})
 	}
 
+	portConfigs := []PortConfig{}
+
+	neighs := []BGPNeighbor{}
+	for connName, conn := range agent.Spec.Connections {
+		if conn.Fabric == nil {
+			continue
+		}
+
+		switch {
+		case isSpine:
+			leafName := ""
+			for _, link := range conn.Fabric.Links {
+				if link.Spine.DeviceName() != agent.Name {
+					continue
+				}
+
+				leafName = link.Leaf.DeviceName()
+				leafIP, err := netip.ParsePrefix(link.Leaf.IP)
+				if err != nil {
+					return nil, fmt.Errorf("parsing conn %s leaf IP %s: %w", connName, link.Leaf.IP, err)
+				}
+				neighs = append(neighs, BGPNeighbor{
+					IP:          leafIP.Addr().String(),
+					PeerGroup:   "underlay_leaf",
+					Description: "fabric underlay to leaf " + link.Leaf.Port,
+				})
+				portConfigs = append(portConfigs, PortConfig{
+					Name:            swp(link.Spine.LocalPortName()),
+					IP:              link.Spine.IP,
+					AdaptiveRouting: true,
+				})
+			}
+
+			leafIP, err := netip.ParsePrefix(agent.Spec.Switches[leafName].ProtocolIP)
+			if err != nil {
+				return nil, fmt.Errorf("parsing sw %s proto IP %s: %w", leafName, agent.Spec.Switches[leafName].ProtocolIP, err)
+			}
+			neighs = append(neighs, BGPNeighbor{
+				IP:          leafIP.Addr().String(),
+				PeerGroup:   "overlay",
+				Description: "fabric overlay to leaf " + leafName,
+			})
+		case isLeaf:
+			spineName := ""
+			for _, link := range conn.Fabric.Links {
+				if link.Leaf.DeviceName() != agent.Name {
+					continue
+				}
+
+				spineName = link.Spine.DeviceName()
+				spineIP, err := netip.ParsePrefix(link.Spine.IP)
+				if err != nil {
+					return nil, fmt.Errorf("parsing conn %s spine IP %s: %w", connName, link.Spine.IP, err)
+				}
+				neighs = append(neighs, BGPNeighbor{
+					IP:          spineIP.Addr().String(),
+					PeerGroup:   "underlay_spine",
+					Description: "fabric underlay to spine " + link.Spine.Port,
+				})
+				portConfigs = append(portConfigs, PortConfig{
+					Name:            swp(link.Leaf.LocalPortName()),
+					IP:              link.Leaf.IP,
+					AdaptiveRouting: true,
+				})
+			}
+
+			spineIP, err := netip.ParsePrefix(agent.Spec.Switches[spineName].ProtocolIP)
+			if err != nil {
+				return nil, fmt.Errorf("parsing sw %s proto IP %s: %w", spineName, agent.Spec.Switches[spineName].ProtocolIP, err)
+			}
+			neighs = append(neighs, BGPNeighbor{
+				IP:          spineIP.Addr().String(),
+				PeerGroup:   "overlay",
+				Description: "fabric overlay to spine " + spineName,
+			})
+		}
+	}
+
+	vpcs := []VPC{}
+	for vpcName, vpc := range agent.Spec.VPCs {
+		if len(vpc.Subnets) != 1 {
+			continue
+		}
+
+		for subnetName, subnet := range vpc.Subnets {
+			vni, ok := agent.Spec.Catalog.GetVPCSubnetVNI(vpcName, subnetName)
+			if !ok {
+				continue
+			}
+
+			vpcs = append(vpcs, VPC{
+				Name: vpcName,
+				VNI:  vni,
+				// TODO make sure to only readvertise those subnets?
+				Subnet: subnet.Subnet,
+			})
+		}
+	}
+
+	for _, attach := range agent.Spec.VPCAttachments {
+		conn, ok := agent.Spec.Connections[attach.Connection]
+		if !ok {
+			continue
+		}
+
+		if conn.Unbundled == nil {
+			continue
+		}
+
+		if conn.Unbundled.Link.Switch.DeviceName() != agent.Name {
+			continue
+		}
+
+		vpcName := attach.VPCName()
+
+		portConfigs = append(portConfigs, PortConfig{
+			Name: swp(conn.Unbundled.Link.Switch.LocalPortName()),
+			VRF:  vpcName,
+		})
+	}
+
+	slices.SortFunc(neighs, func(a, b BGPNeighbor) int {
+		// not ideal, but gives stable ordering
+		return strings.Compare(a.IP, b.IP)
+	})
+	slices.SortFunc(vpcs, func(a, b VPC) int {
+		return strings.Compare(a.Name, b.Name)
+	})
+	slices.SortFunc(portConfigs, func(a, b PortConfig) int {
+		// not ideal, but gives stable ordering
+		return strings.Compare(a.Name, b.Name)
+	})
+
 	cfgIn := ConfigIn{
 		Hostname:     agent.Name,
-		ManagementIP: agent.Spec.Switch.IP,
 		Users:        users,
 		NTPServer:    controlVIP.Addr().String(),
+		ManagementIP: agent.Spec.Switch.IP,
+		ProtocolIP:   agent.Spec.Switch.ProtocolIP,
+		VTEPIP:       agent.Spec.Switch.VTEPIP,
+		ASN:          agent.Spec.Switch.ASN,
+		RouterID:     protocolIP.Addr().String(),
+		VXLANSource:  vtepIP.Addr().String(),
+		VPCs:         vpcs,
+		BGPNeighbors: neighs,
+		PortConfigs:  portConfigs,
+		IsSpine:      isSpine,
+		IsLeaf:       isLeaf,
+
+		// TODO remove hard-coded value and properly handle
+		HostSubnet: "10.0.0.0/8",
+
+		// TODO: keep ports down by default
 	}
 
 	// TODO cache template
@@ -114,6 +307,19 @@ func buildConfigFor(tmpl string, agent *agentapi.Agent) (*bytes.Buffer, error) {
 	}
 
 	return cfgBuf, nil
+}
+
+func swp(in string) string {
+	if portStr, ok := strings.CutPrefix(in, "E1/"); ok {
+		port, err := strconv.ParseUint(portStr, 10, 16)
+		if err != nil {
+			return "port-invalid-uint"
+		}
+
+		return fmt.Sprintf("swp%d", port)
+	}
+
+	return "port-invalid-prefix"
 }
 
 //go:embed ztp_script.tmpl.sh
@@ -165,7 +371,10 @@ func BuildZTPFor(agent *agentapi.Agent, kubeConfig []byte) (*bytes.Buffer, error
 }
 
 func Enforce(ctx context.Context, _ /* processor */ dozer.Processor, agent *agentapi.Agent, basedir string, dryRun bool) error {
-	// TODO generate config into temp revision and try diff on it
+	cfgBuf, err := buildConfigFor(fullCfgTmpl, agent)
+	if err != nil {
+		return fmt.Errorf("building full config: %w", err)
+	}
 
 	if dryRun {
 		slog.Warn("Dry run, exiting")
@@ -175,11 +384,6 @@ func Enforce(ctx context.Context, _ /* processor */ dozer.Processor, agent *agen
 
 	ctx, cancel := context.WithTimeout(ctx, 5*time.Minute)
 	defer cancel()
-
-	cfgBuf, err := buildConfigFor(fullCfgTmpl, agent)
-	if err != nil {
-		return fmt.Errorf("building full config: %w", err)
-	}
 
 	cfgPath := filepath.Join(basedir, cfgFile)
 	if err := os.WriteFile(cfgPath, cfgBuf.Bytes(), 0o644); err != nil { //nolint:gosec
@@ -192,8 +396,10 @@ func Enforce(ctx context.Context, _ /* processor */ dozer.Processor, agent *agen
 		return fmt.Errorf("replacing config: %w", err)
 	}
 
+	// Diff exit code is 1 if it's non-empty
 	if err := run(ctx, "nv-diff", "nv", "config", "diff", "applied"); err != nil {
-		return fmt.Errorf("diffing config: %w", err)
+		// return fmt.Errorf("diffing config: %w", err)
+		slog.Debug("Diff", "err", err.Error())
 	}
 
 	if err := run(ctx, "nv-apply", "nv", "config", "apply", "-y"); err != nil {
