af4d9cdfe284c64fa3a33850f53bbed347949643
Logan Blyth <logan@githedgehog.com>
2026-05-11 17:06:37 -0400

    feat: add AS4630-54-PE aka dcs202
    Signed-off-by: Logan Blyth <logan@githedgehog.com>

---
+++ pkg/ctrl/switchprofile/p_bcm_edgecore_eps202.go
@@ -0,0 +1,121 @@
+// Copyright 2025 Hedgehog
+// SPDX-License-Identifier: Apache-2.0
+
+package switchprofile
+
+import (
+	"go.githedgehog.com/fabric/api/meta"
+	wiringapi "go.githedgehog.com/fabric/api/wiring/v1beta1"
+	kmetav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+var EdgecoreEPS202 = wiringapi.SwitchProfile{
+	ObjectMeta: kmetav1.ObjectMeta{
+		Name: "edgecore-eps202",
+	},
+	Spec: wiringapi.SwitchProfileSpec{
+		DisplayName:   "Edgecore EPS202",
+		OtherNames:    []string{"Edgecore AS4630-54PE"},
+		SwitchSilicon: SiliconBroadcomTD3_X3,
+		Features: wiringapi.SwitchProfileFeatures{
+			Subinterfaces: false,
+			ACLs:          true,
+			L2VNI:         true,
+			L3VNI:         true,
+			RoCE:          false,
+			MCLAG:         false,
+			ESLAG:         true,
+			ECMPRoCEQPN:   false,
+		},
+		Notes:    "Doesn't support StaticExternals and ExternalAttachments with VLANs due to the lack of subinterfaces support.",
+		NOSType:  meta.NOSTypeSONiCBCMCampus,
+		Platform: "x86_64-accton_as4630_54pe-r0",
+		Config: wiringapi.SwitchProfileConfig{
+			MaxPathsEBGP: 16,
+		},
+		Ports: map[string]wiringapi.SwitchProfilePort{
+			"M1":    {NOSName: "Management0", Management: true, OniePortName: "eth2"},
+			"E1/1":  {NOSName: "Ethernet0", Label: "1", Profile: "RJ45-1G"},
+			"E1/2":  {NOSName: "Ethernet1", Label: "2", Profile: "RJ45-1G"},
+			"E1/3":  {NOSName: "Ethernet2", Label: "3", Profile: "RJ45-1G"},
+			"E1/4":  {NOSName: "Ethernet3", Label: "4", Profile: "RJ45-1G"},
+			"E1/5":  {NOSName: "Ethernet4", Label: "5", Profile: "RJ45-1G"},
+			"E1/6":  {NOSName: "Ethernet5", Label: "6", Profile: "RJ45-1G"},
+			"E1/7":  {NOSName: "Ethernet6", Label: "7", Profile: "RJ45-1G"},
+			"E1/8":  {NOSName: "Ethernet7", Label: "8", Profile: "RJ45-1G"},
+			"E1/9":  {NOSName: "Ethernet8", Label: "9", Profile: "RJ45-1G"},
+			"E1/10": {NOSName: "Ethernet9", Label: "10", Profile: "RJ45-1G"},
+			"E1/11": {NOSName: "Ethernet10", Label: "11", Profile: "RJ45-1G"},
+			"E1/12": {NOSName: "Ethernet11", Label: "12", Profile: "RJ45-1G"},
+			"E1/13": {NOSName: "Ethernet12", Label: "13", Profile: "RJ45-1G"},
+			"E1/14": {NOSName: "Ethernet13", Label: "14", Profile: "RJ45-1G"},
+			"E1/15": {NOSName: "Ethernet14", Label: "15", Profile: "RJ45-1G"},
+			"E1/16": {NOSName: "Ethernet15", Label: "16", Profile: "RJ45-1G"},
+			"E1/17": {NOSName: "Ethernet16", Label: "17", Profile: "RJ45-1G"},
+			"E1/18": {NOSName: "Ethernet17", Label: "18", Profile: "RJ45-1G"},
+			"E1/19": {NOSName: "Ethernet18", Label: "19", Profile: "RJ45-1G"},
+			"E1/20": {NOSName: "Ethernet19", Label: "20", Profile: "RJ45-1G"},
+			"E1/21": {NOSName: "Ethernet20", Label: "21", Profile: "RJ45-1G"},
+			"E1/22": {NOSName: "Ethernet21", Label: "22", Profile: "RJ45-1G"},
+			"E1/23": {NOSName: "Ethernet22", Label: "23", Profile: "RJ45-1G"},
+			"E1/24": {NOSName: "Ethernet23", Label: "24", Profile: "RJ45-1G"},
+			"E1/25": {NOSName: "Ethernet24", Label: "25", Profile: "RJ45-1G"},
+			"E1/26": {NOSName: "Ethernet25", Label: "26", Profile: "RJ45-1G"},
+			"E1/27": {NOSName: "Ethernet26", Label: "27", Profile: "RJ45-1G"},
+			"E1/28": {NOSName: "Ethernet27", Label: "28", Profile: "RJ45-1G"},
+			"E1/29": {NOSName: "Ethernet28", Label: "29", Profile: "RJ45-1G"},
+			"E1/30": {NOSName: "Ethernet29", Label: "30", Profile: "RJ45-1G"},
+			"E1/31": {NOSName: "Ethernet30", Label: "31", Profile: "RJ45-1G"},
+			"E1/32": {NOSName: "Ethernet31", Label: "32", Profile: "RJ45-1G"},
+			"E1/33": {NOSName: "Ethernet32", Label: "33", Profile: "RJ45-1G"},
+			"E1/34": {NOSName: "Ethernet33", Label: "34", Profile: "RJ45-1G"},
+			"E1/35": {NOSName: "Ethernet34", Label: "35", Profile: "RJ45-1G"},
+			"E1/36": {NOSName: "Ethernet35", Label: "36", Profile: "RJ45-1G"},
+			"E1/37": {NOSName: "Ethernet36", Label: "37", Profile: "RJ45-1G"},
+			"E1/38": {NOSName: "Ethernet37", Label: "38", Profile: "RJ45-1G"},
+			"E1/39": {NOSName: "Ethernet38", Label: "39", Profile: "RJ45-1G"},
+			"E1/40": {NOSName: "Ethernet39", Label: "40", Profile: "RJ45-1G"},
+			"E1/41": {NOSName: "Ethernet40", Label: "41", Profile: "RJ45-1G"},
+			"E1/42": {NOSName: "Ethernet41", Label: "42", Profile: "RJ45-1G"},
+			"E1/43": {NOSName: "Ethernet42", Label: "43", Profile: "RJ45-1G"},
+			"E1/44": {NOSName: "Ethernet43", Label: "44", Profile: "RJ45-1G"},
+			"E1/45": {NOSName: "Ethernet44", Label: "45", Profile: "RJ45-1G"},
+			"E1/46": {NOSName: "Ethernet45", Label: "46", Profile: "RJ45-1G"},
+			"E1/47": {NOSName: "Ethernet46", Label: "47", Profile: "RJ45-1G"},
+			"E1/48": {NOSName: "Ethernet47", Label: "48", Profile: "RJ45-1G"}, // 48x RJ45 1G
+			"E1/49": {NOSName: "Ethernet48", Label: "49", Profile: "SFP28-25G"},
+			"E1/50": {NOSName: "Ethernet49", Label: "50", Profile: "SFP28-25G"},
+			"E1/51": {NOSName: "Ethernet50", Label: "51", Profile: "SFP28-25G"},
+			"E1/52": {NOSName: "Ethernet51", Label: "52", Profile: "SFP28-25G"}, // 4x SFP28 25G
+			"E1/53": {NOSName: "1/53", BaseNOSName: "Ethernet52", Label: "53", Profile: "QSFP28-100G"},
+			"E1/54": {NOSName: "1/54", BaseNOSName: "Ethernet56", Label: "54", Profile: "QSFP28-100G"}, // 2x QSFP28 100G
+		},
+		PortProfiles: map[string]wiringapi.SwitchProfilePortProfile{
+			"RJ45-1G": {
+				Speed: &wiringapi.SwitchProfilePortProfileSpeed{
+					Default:   "1G",
+					Supported: []string{"1G"},
+				},
+				AutoNegAllowed: true,
+				AutoNegDefault: true,
+			},
+			"SFP28-25G": {
+				Speed: &wiringapi.SwitchProfilePortProfileSpeed{
+					Default:   "25G",
+					Supported: []string{"1G", "10G", "25G"},
+				},
+			},
+			"QSFP28-100G": {
+				Breakout: &wiringapi.SwitchProfilePortProfileBreakout{
+					Default: "1x100G",
+					Supported: map[string]wiringapi.SwitchProfilePortProfileBreakoutMode{
+						"1x40G":  {Offsets: []string{"0"}},
+						"1x100G": {Offsets: []string{"0"}},
+						"4x10G":  {Offsets: []string{"0", "1", "2", "3"}},
+						"4x25G":  {Offsets: []string{"0", "1", "2", "3"}},
+					},
+				},
+			},
+		},
+	},
+}
